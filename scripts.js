61ff428a3efda5183218e22cf464283aaa6f434b
stopmotio <homeyone2387@gmail.com>
2021-07-17 11:06:15 -0400

    Fix some issues from yday's commit, new footer, less CSS loaded.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,3 @@
-
-
-
-
-// THIRD PARTY CODE
-// https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-/*
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-/*ABOVE IS LEGACY!*/
-
 function NavToggle() {
   var nav = document.getElementById('navlist');
   if(nav.style.display == "none") {
